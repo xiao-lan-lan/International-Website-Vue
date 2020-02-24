b65d8322c676afc86cfa7be111258178f5b7c7f4
兰小兰 <479323417@qq.com>
2020-02-24 22:30:23 +0800

    :rocket: router

---
+++ src/views/Layout/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-header class="header">Header</el-header>
+      <router-view />
+      <el-footer class="footer">Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
+
+<style lang="less" scoped>
+.header,.footer {
+  width: 100%;
+  height: 60px;
+  background-color: #253237;
+  color: #fff;
+}
+.header {
+  position: fixed;
+  top: 0;
+}
+.footer {
+  position: fixed;
+  bottom: 0;
+}
+</style>
